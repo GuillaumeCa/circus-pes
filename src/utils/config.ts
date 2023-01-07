46944819308af2581c1362853eaf78a2f3577376
GuillaumeCa <guicarre1@gmail.com>
2023-01-07 12:41:47 +0100

    change port for image endpoint

--- src/utils/config.ts
+++ src/utils/config.ts
@@ -1,5 +1,5 @@
 export const IMAGE_BUCKET_NAME =
   process.env.NEXT_PUBLIC_MINIO_IMAGE_BUCKET_NAME || "circuspes-images";
 export const STORAGE_BASE_URL = `https://${
-  process.env.NEXT_PUBLIC_MINIO_ENDPOINT || "localhost"
-}:9000/${IMAGE_BUCKET_NAME}/`;
+  process.env.NEXT_PUBLIC_MINIO_ENDPOINT || ""
+}/${IMAGE_BUCKET_NAME}/`;
