4a1a349c9fcdfe57520db774d2a619d387361775
GuillaumeCa <guicarre1@gmail.com>
2023-01-09 16:08:14 +0100

    fix build

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -5,7 +5,7 @@ export default function FatalError() {
     <BaseLayout>
       <div className="text-center">
         <h2 className="text-4xl font-bold m-8">
-          Oops, quelque chose s'est mal passé
+          Oops, quelque chose s&apos;est mal passé
         </h2>
         <p className="text-gray-400 text-lg">
           Veuillez réessayer dans un instant
